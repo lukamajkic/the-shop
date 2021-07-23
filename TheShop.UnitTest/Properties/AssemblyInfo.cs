30554d661f833670c357b80b80d30e321c8430e9
Luka Majkic <lukam@yuspin.rs>
2021-07-23 11:21:41 +0200

    Added unit test project; Wrote unit tests for ordering and selling;

---
+++ TheShop.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TheShop.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TheShop.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("88e4bdeb-e1d0-4682-9233-50bd9e9f07e5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TheShop.UnitTest/ShopTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TheShop.UnitTest
+{
+    [TestClass]
+    public class ShopTest
+    {
+        ShopService shopService;
+        List<Supplier> suppliers = new List<Supplier>();
+
+        [TestMethod]
+        public void OrderSuccessfulTest()
+        {
+            InitializeConfiguration();
+            int orderId = 1;
+            int orderMaxPrice = 500;
+            var article = shopService.OrderArticle(orderId, orderMaxPrice);
+            Assert.IsNotNull(article,"Should not be null");
+        }
+
+         [TestMethod]
+        public void OrderFailedTest()
+        {
+            InitializeConfiguration();
+            int orderId = 1;
+            int orderMaxPrice = 400;
+            var article = shopService.OrderArticle(orderId, orderMaxPrice);
+            Assert.IsNull(article,"Should be null");
+        }
+
+        [TestMethod]
+        public void SellTest()
+        {
+            InitializeConfiguration();
+            var article = new Article { ID = 1, ArticlePrice = 10 };
+            int buyerId = 10;
+            shopService.SellArticle(buyerId, article);
+            var resultArticle = shopService.GetById(article.ID);
+            Assert.IsTrue(resultArticle.IsSold, "Should be true");
+        }
+
+        private void InitializeConfiguration()
+        {
+            int supplier1Number = 1;
+            int supplier2Number = 1;
+            int supplier3Number = 1;
+            int supplier1ArticlePrice = 458;
+            int supplier2ArticlePrice = 459;
+            int supplier3ArticlePrice = 460;
+            int supplier1ArticleId = 1;
+            int supplier2ArticleId = 1;
+            int supplier3ArticleId = 1;
+
+            var supplier1 = new Supplier(supplier1Number, supplier1ArticlePrice, supplier1ArticleId);
+            var supplier2 = new Supplier(supplier2Number, supplier2ArticlePrice, supplier2ArticleId);
+            var supplier3 = new Supplier(supplier3Number, supplier3ArticlePrice, supplier3ArticleId);
+            suppliers.Add(supplier1);
+            suppliers.Add(supplier2);
+            suppliers.Add(supplier3);
+            var databaseDriver = new DatabaseDriver();
+            var logger = new Logger();
+
+            shopService = new ShopService(databaseDriver, logger, supplier1, supplier2, supplier3);
+        }
+    }
+}
